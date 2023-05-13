d091955ed3cb74d9f87abaa8c9edaa602ebb8355
Ahmed <ahmed.ovicsaleh@outlook.com>
2023-05-13 19:57:45 +0300

    rearrange the table component to be more readable

--- src/component/Table/Table.js
+++ src/component/Table/Table.js
@@ -1,43 +1,10 @@
 import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { data } from "../../data";
+import { data  as dataSet} from "../../data";
 import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
-import { Line } from "react-chartjs-2";
-import { predict } from "../../mock/api/predict";
-
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 800,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
+import PredictionChart from "../LineChart/PredictionChart";
+import styles from "./styles.module.css"
 
 export default function Table() {
   const [open, setOpen] = React.useState(false);
@@ -46,70 +13,6 @@ export default function Table() {
   const handleOpen = (row) => {
     setName(row.Name);
     setOpen(true);
-  };
-
-  const result = predict.result;
-
-  const labels = result.map((item) => {
-    return Object.keys(item);
-  });
-
-  const options = {
-    scales: {
-      x: {
-        ticks: {
-          font: {
-            size: 14,
-          },
-          color: "black",
-        },
-      },
-    },
-    y: {
-      ticks: {
-        font: {
-          size: 15,
-        },
-        color: "black",
-      },
-    },
-
-    responsive: true,
-    plugins: {
-      legend: {
-        position: "top",
-      },
-      title: {
-        display: true,
-        text: `${name}`,
-        font: {
-          size: 25,
-          family: "Poppins, sans-serif",
-        },
-      },
-    },
-  };
-
-  const predictdata = {
-    labels,
-    datasets: [
-      {
-        label: "actual",
-        borderColor: "rgb(53, 162, 235)",
-        data: result.map((item) => {
-          return item[Object.keys(item)[0]].actual;
-        }),
-        backgroundColor: "rgba(53, 162, 235, 0.5)",
-      },
-      {
-        label: "predict",
-        borderColor: "rgb(255, 99, 132)",
-        data: result.map((item) => {
-          return item[Object.keys(item)[0]].predict;
-        }),
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-    ],
   };
 
   return (
@@ -123,11 +26,11 @@ export default function Table() {
           },
         }}
         pageSizeOptions={[5, 15, 20]}
-        rows={data.map((item) => {
+        rows={dataSet.map((item) => {
           return { id: item.id, Name: item.name, price: item.price };
         })}
         columns={[
-          { field: "id", headerName: "ID", width: 150 },
+          { field: "id", headerName: "ID", width: 150},
           { field: "Name", headerName: "Name", width: 300 },
           { field: "price", headerName: "Price", width: 150 },
           {
@@ -155,17 +58,13 @@ export default function Table() {
           margin: "auto",
         }}
       />
-      {
+
         <Modal open={open} onClose={handleClose}>
-          <Box sx={style}>
-            <Line
-              style={{ width: "100%" }}
-              options={options}
-              data={predictdata}
-            />
-          </Box>
+          <div className={styles.chart} >
+            <PredictionChart name={name}/>
+          </div>
         </Modal>
-      }
+
     </div>
   );
 }
